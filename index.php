b7cba2fc97c1cc8f8aec1f7aa9ba637e16db4c72
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:23:06 +0200

    php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>exo3</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $km = '1';
+    echo "je fais $km km.\n";
+    $km = '3';
+    echo "Maintenant je fais $km km.\n";
+    $km = '125';
+    echo "Maintenant je fais $km km.\n";
+     ?>
+   </p>
+  </body>
+</html>
